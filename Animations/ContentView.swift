fbcecdbc44b089f709b2539652451a88ef65de0c
aaryankotharii <aaryan.kothari@gmail.com>
2020-02-24 01:03:01 +0530

    initial commit

---
+++ Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Aaryan Kothari on 24/02/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Text("Hello, World!")
+            Image(systemName: "person.fill")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
