62ca6cb485846131479980371b2db3d3bd6f0ddb
aaryankotharii <aaryan.kothari@gmail.com>
2020-02-24 23:10:20 +0530

    this is boring, will do later!!

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,31 +11,31 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount : CGFloat = 1
     var body: some View {
-        VStack {
-            Spacer()
-            Stepper("stepper",value: $animationAmount.animation(
-                Animation.easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false)
-            ), in: 1...10)
-            Spacer()
-            Button("Tap me"){
-                self.animationAmount += 1
-            }
-            .padding(50)
-            .background(Color.red)
-            .foregroundColor(Color.white)
-            .clipShape(Circle())
-//        .overlay(
-//            Circle()
-//                .stroke(Color.red)
-            .scaleEffect(animationAmount)
-//            .opacity(Double(2-animationAmount))
-//            .animation(
+//        VStack {
+//            Spacer()
+//            Stepper("stepper",value: $animationAmount.animation(
 //                Animation.easeInOut(duration: 1)
-//                .repeatForever(autoreverses: false)
-//            )
-//            )
-        }
+//                    .repeatForever(autoreverses: false)
+//            ), in: 1...10)
+//            Spacer()
+//            Button("Tap me"){
+//                self.animationAmount += 1
+//            }
+//            .padding(50)
+//            .background(Color.red)
+//            .foregroundColor(Color.white)
+//            .clipShape(Circle())
+////        .overlay(
+////            Circle()
+////                .stroke(Color.red)
+//            .scaleEffect(animationAmount)
+////            .opacity(Double(2-animationAmount))
+////            .animation(
+////                Animation.easeInOut(duration: 1)
+////                .repeatForever(autoreverses: false)
+////            )
+////            )
+//        }
   
     }
 }
