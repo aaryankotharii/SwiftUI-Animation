17b069ce860b8de939467f2312f7587193d51a7f
aaryankotharii <aaryan.kothari@gmail.com>
2020-02-24 23:09:15 +0530

    some more stuff

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,25 +11,30 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount : CGFloat = 1
     var body: some View {
-        Button("Tap me"){
-            self.animationAmount *= 1.2
-        }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(Color.white)
-        .clipShape(Circle())
-    .overlay(
-        Circle()
-            .stroke(Color.red)
-        .scaleEffect(animationAmount)
-        .opacity(Double(2-animationAmount))
-        .animation(
-            Animation.easeInOut(duration: 1)
-            .repeatForever(autoreverses: false)
-        )
-        )
-            .onAppear {
-                self.animationAmount = 2
+        VStack {
+            Spacer()
+            Stepper("stepper",value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false)
+            ), in: 1...10)
+            Spacer()
+            Button("Tap me"){
+                self.animationAmount += 1
+            }
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(Color.white)
+            .clipShape(Circle())
+//        .overlay(
+//            Circle()
+//                .stroke(Color.red)
+            .scaleEffect(animationAmount)
+//            .opacity(Double(2-animationAmount))
+//            .animation(
+//                Animation.easeInOut(duration: 1)
+//                .repeatForever(autoreverses: false)
+//            )
+//            )
         }
   
     }
